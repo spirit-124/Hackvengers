43643196ede0db92bc9808c84b407f8737f44d57
Bajrang Agrawal <bajrangofficial580@gmail.com>
2023-02-05 14:27:53 +0530

    [added] socila ICons

---
+++ index.js
@@ -0,0 +1,26 @@
+// sponserBtn.addEventListener("click", function () {
+//   document.location.href = "https://forms.gle/UjCT6LfjXCXEpAXK6";
+// });
+
+// var cursor = document.querySelector(".cursor"),
+//   cursorScale = document.querySelectorAll(".cursor-scale"),
+//   mouseX = 0,
+//   mouseY = 0;
+
+// gsap.to({}, 0.016, {
+//   repeat: -1,
+
+//   onRepeat: function () {
+//     gsap.set(cursor, {
+//       css: {
+//         left: mouseX,
+//         top: mouseY,
+//       },
+//     });
+//   },
+// });
+
+// window.addEventListener("mousemove", function (e) {
+//   mouseX = e.clientX;
+//   mouseY = e.clientY;
+// });
